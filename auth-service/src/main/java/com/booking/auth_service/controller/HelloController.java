fb71ba6c7594c944e82c04ceaa931c44cce498e3
olga <olga_afoninaaa@inbox.ru>
2026-06-29 18:51:23 +0300

    feat: added helloControllers and API Gateway

---
+++ auth-service/src/main/java/com/booking/auth_service/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.booking.auth_service.controller;
+
+public class HelloController {
+}
---
+++ notifaction-service/src/main/java/com/booking/notifaction_service/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.booking.auth_service.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @GetMapping
+    public String hello(){
+        return "Hello my name: " + serviceName + " v1";
+    }
+
+}
