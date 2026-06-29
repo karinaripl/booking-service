3702825576b52b1812c2b2fc78b054154ba01f98
olga <olga_afoninaaa@inbox.ru>
2026-06-29 17:02:50 +0300

    feat: added basic project structure

---
+++ gateway-auth-starter/src/main/java/com/booking/gateway_auth_starter/GatewayAuthStarterApplication.java
@@ -0,0 +1,13 @@
+package com.booking.gateway_auth_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAuthStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAuthStarterApplication.class, args);
+	}
+
+}
---
+++ gateway-auth-starter/src/test/java/com/booking/gateway_auth_starter/GatewayAuthStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.gateway_auth_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAuthStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
